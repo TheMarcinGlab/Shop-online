e6c73909fa6c015bc856abae5fbce0750e5f3ccb
Marcin Głąb <marcinglabppx@gmail.com>
2024-12-09 20:56:08 +0100

    Ukończony projekt

---
+++ frontend/angular-ecommerce/src/app/components/login-status/login-status.component.ts
@@ -0,0 +1,40 @@
+import { Component, Inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { DOCUMENT } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-status',
+  templateUrl: './login-status.component.html',
+  styleUrls: ['./login-status.component.css']
+})
+export class LoginStatusComponent {
+
+  user$ = this.auth.user$; // Observable z danymi użytkownika
+  storage: Storage = sessionStorage;
+
+  constructor(
+    public auth: AuthService,
+    @Inject(DOCUMENT) private doc: Document,
+    private router: Router
+  ) {}
+
+  loginWithRedirect(): void {
+    this.auth.loginWithRedirect();
+  }
+
+  logout(): void {
+    this.auth.logout({ returnTo: this.doc.location.origin }); // Wylogowanie użytkownika
+    this.router.navigate(['/login']); // Nawigacja po wylogowaniu
+  }
+
+  ngOnInit(): void {
+    // Subskrybuj dane użytkownika i zapisz adres e-mail w sessionStorage
+    this.user$.subscribe(user => {
+      if (user && user.email) {
+        this.storage.setItem('userEmail', user.email);
+        console.log('User email saved to sessionStorage:', user.email);
+      }
+    });
+  }
+}
